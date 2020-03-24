63ef0a42a0982a6d2421be2d182fc5b28001e49e
Joao Martins <joao.lopes.martins@tecnico.ulisboa.pt>
2020-03-24 19:11:52 +0000

    PpA-F2.1: Approval/Rejection of pending questions - test web service update

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/question/service/ApprovalRejectionQuestionPerformanceTest.groovy
@@ -0,0 +1,69 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.question.service
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.QuestionService
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Question
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.repository.QuestionRepository
+import spock.lang.Specification
+
+@DataJpaTest
+class ApprovalRejectionQuestionPerformanceTest extends Specification {
+    public static final String QUESTION_TITLE = "qTitle"
+    public static final String QUESTION_CONTENT = "qContent"
+    public static final String OPTION_CONTENT = "optContent"
+    public static final String JUSTIFICATION_CONTENT = "justification content"
+
+    @Autowired
+    QuestionService questionService
+
+    @Autowired
+    QuestionRepository questionRepository
+
+    def "performance test to approve 5000 questions"() {
+        when:
+        1.upto(5000, {
+            def question
+            question = new Question()
+            question.setId(it)
+            question.setKey(it)
+            question.setTitle(QUESTION_TITLE)
+            question.setContent(QUESTION_CONTENT)
+            question.setStatus(Question.Status.PENDING)
+            questionRepository.save(question)
+
+            questionService.approveQuestion(it, JUSTIFICATION_CONTENT)})
+
+        then:
+        true
+    }
+
+    def "performance test to reject 5000 questions"() {
+        when:
+        1.upto(5000, {
+            def question
+            question = new Question()
+            question.setId(it)
+            question.setKey(it)
+            question.setTitle(QUESTION_TITLE)
+            question.setContent(QUESTION_CONTENT)
+            question.setStatus(Question.Status.PENDING)
+            questionRepository.save(question)
+
+            questionService.rejectQuestion(it, JUSTIFICATION_CONTENT)})
+
+        then:
+        true
+    }
+
+    @TestConfiguration
+    static class QuestionServiceImplTestContextConfiguration {
+
+        @Bean
+        QuestionService questionService() {
+            return new QuestionService()
+        }
+    }
+}
