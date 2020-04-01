f8ab80674bdef60eb7572c9faebe45e56a39aa9f
Manuel Goulão <msilvagoulao@gmail.com>
2020-04-01 19:50:50 +0100

    Open Tourneys - set up routing and page closes #117

---
+++ frontend/src/views/student/tourney/OpenTourneys.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <h2>Open Tourneys List</h2>
+
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+
+@Component
+export default class OpenTourneys extends Vue {
+
+}
+</script>
+
+<style lang="scss" scoped></style>
