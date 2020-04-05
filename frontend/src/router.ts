7a55483f9dde68142f432da6748302555549b92c
xaviergomes99 <xavycg99@gmail.com>
2020-04-05 17:16:25 +0100

    PpA-F1.1:Submit Question - set up routing and pages closes #120

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -18,6 +18,7 @@ import QuizView from './views/student/quiz/QuizView.vue';
 import ResultsView from './views/student/quiz/ResultsView.vue';
 import StatsView from './views/student/StatsView.vue';
 import ScanView from './views/student/ScanView.vue';
+import SubmitQuestionView from './views/student/submissions/SubmitQuestionView.vue';
 
 import AdminManagementView from './views/admin/AdminManagementView.vue';
 import NotFoundView from './views/NotFoundView.vue';
@@ -182,6 +183,15 @@ let router = new Router({
           component: ScanView,
           meta: {
             title: process.env.VUE_APP_NAME + ' - Scan',
+            requiredAuth: 'Student'
+          }
+        },
+        {
+          path: 'submit',
+          name: 'submit',
+          component: SubmitQuestionView,
+          meta: {
+            title: process.env.VUE_APP_NAME + ' - Submit',
             requiredAuth: 'Student'
           }
         }
---
+++ frontend/src/views/student/submissions/SubmitQuestionView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Submit Questions View</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class SubmitQuestionView extends Vue {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
