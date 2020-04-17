9b4a42d7729e966380931d72d9447e4d6420f834
xaviergomes99 <xavycg99@gmail.com>
2020-04-17 16:57:02 +0100

    added cypress tests that was missing due to gitignore

---
+++ frontend/cypress/integration/student/submitQuestions.js
@@ -0,0 +1,33 @@
+describe('Question Submission walkthrough', () => {
+    beforeEach(() => {
+        cy.demoStudentLogin()
+    })
+
+    afterEach(() => {
+        cy.contains('Logout').click()
+    })
+
+    it('login, submits and deletes a question', () => {
+        cy.submitQuestion('FE Test Question Title', 'FE Test Question Content', 'FE Test Question Option Content')
+
+        cy.deleteSubmittedQuestion('FE Test Question Title')
+    });
+
+    it('login, try to submit a question without a title and cancel the submission', () => {
+        cy.log('try to submit without a title')
+        cy.submitQuestion('   ', 'FE Test Question Content', 'FE Test Question Option Content')
+
+        cy.closeErrorMessage()
+
+        cy.log('close dialog')
+        cy.get('[data-cy="cancelButton"]').click()
+    });
+
+    it('login, submits two questions and deletes them', () => {
+        cy.submitQuestion('FE Test Question Title 1', 'FE Test Question Content', 'FE Test Question Option Content')
+        cy.submitQuestion('FE Test Question Title 2', 'FE Test Question Content', 'FE Test Question Option Content')
+
+        cy.deleteSubmittedQuestion('FE Test Question Title 1')
+        cy.deleteSubmittedQuestion('FE Test Question Title 2')
+    });
+});
