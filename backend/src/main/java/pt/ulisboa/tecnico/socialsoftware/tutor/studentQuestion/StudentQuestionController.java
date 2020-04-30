1edbe7697e7e89b5c342e1954cf846bf495ee167
xaviergomes99 <xavycg99@gmail.com>
2020-04-30 18:04:30 +0100

    PpA-F4.1: define and test web service closes #165

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/studentQuestion/StudentQuestionController.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/studentQuestion/StudentQuestionController.java
@@ -62,7 +62,7 @@ public class StudentQuestionController {
 
     @PostMapping("/courses/{courseId}/questionsFromStudents")
     @PreAuthorize("hasRole('ROLE_TEACHER') and hasPermission(#courseId, 'COURSE.ACCESS')")
-    public QuestionDto makeStudentQuestionAvailable(@PathVariable int courseId, @RequestBody Integer studentQuestionId) {
+    public QuestionDto makeStudentQuestionAvailable(@PathVariable int courseId, @RequestBody int studentQuestionId) {
 
         return this.studentQuestionService.makeStudentQuestionAvailable(studentQuestionId);
     }
